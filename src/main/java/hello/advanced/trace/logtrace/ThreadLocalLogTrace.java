e6355b2314a1e8a7427fe7df37ba78bf2739092b
DESKTOP-VSDKPQV\tmdgh <tmdghks7836@naver.com>
2022-01-03 21:22:01 +0900

    no message

---
+++ src/main/java/hello/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -0,0 +1,74 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocalLogTrace implements LogTrace {
+
+    private static final String START_PREFIX = "--->";
+    private static final String COMPLETE_PREFIX = "<---";
+    private static final String EX_PREFIX = "<X--";
+
+    private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>(); //traceId 동기화, 동시성 이슈
+
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = traceIdHolder.get();
+        long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    private void syncTraceId() {
+
+        TraceId traceId = traceIdHolder.get();
+        if (traceId == null) {
+            traceIdHolder.set(new TraceId());
+        } else {
+            traceIdHolder.set(traceId.createNextId());
+        }
+    }
+
+    public void end(TraceStatus traceStatus) {
+        complete(traceStatus, null);
+    }
+
+    public void exception(TraceStatus traceStatus, Exception e) {
+        complete(traceStatus, e);
+    }
+
+    private void complete(TraceStatus traceStatus, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - traceStatus.getStartTimeMs();
+        TraceId traceId = traceStatus.getTraceId();
+
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), traceStatus.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}: 예외발생!!", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), traceStatus.getMessage(), resultTimeMs, e.getClass());
+        }
+
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+
+        TraceId traceId = traceIdHolder.get();
+        if (traceId.isFirstLevel()) {
+            traceIdHolder.remove();
+        } else {
+            traceIdHolder.set(traceId.createPreviousId());
+        }
+    }
+
+    private Object addSpace(String startPrefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + startPrefix : "|   ");
+        }
+        return sb.toString();
+    }
+
+}
