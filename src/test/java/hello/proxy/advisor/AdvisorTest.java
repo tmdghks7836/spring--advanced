bc0c9554ee223c761875a396d6fc3058187b58c2
이승환 <tmdghks7836@naver.com>
2022-01-14 16:56:00 +0900

    no message

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -1,4 +1,26 @@
 package hello.proxy.advisor;
 
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
 public class AdvisorTest {
+
+    @Test
+    void advisorTest1(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory factory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        factory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) factory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
 }
